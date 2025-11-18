f205c5cf0c5652e50652d68f7cce78491a06af56
l3d_team4 <l3d_team4@riddle.autonomy.ri.cmu.edu>
2025-11-18 19:27:53 +0000

    Added modality transforms, able to train but runs out of memory

--- getting_started/kinova.py
+++ getting_started/kinova.py
@@ -13,6 +13,22 @@ from transformers import TrainingArguments
 
 from gr00t.experiment.runner import TrainRunner
 
+from gr00t.data.dataset import ModalityConfig
+from gr00t.data.transform.base import ComposedModalityTransform, ModalityTransform
+from gr00t.data.transform.concat import ConcatTransform
+from gr00t.data.transform.state_action import (
+    StateActionSinCosTransform,
+    StateActionToTensor,
+    StateActionTransform,
+)
+from gr00t.data.transform.video import (
+    VideoColorJitter,
+    VideoCrop,
+    VideoResize,
+    VideoToNumpy,
+    VideoToTensor,
+)
+from gr00t.model.transforms import GR00TTransform
 # REPO_PATH is the path of the pip install gr00t repo and one level up
 REPO_PATH = os.path.dirname(os.path.dirname(gr00t.__file__))
 DATA_PATH = os.path.join(REPO_PATH, "datasets/kinova_dataset_nov6")
@@ -21,37 +37,97 @@ print("Loading dataset... from", DATA_PATH)
 
 # 2. modality configs
 
-modality_configs = {
+video_keys = ["video.external", "video.wrist"]
+state_keys = ["state.arm_joints", "state.gripper"]
+action_keys = ["action.arm_joints", "action.gripper"]
+language_keys = ["annotation.human.task_description"]
+observation_indices = [0]
+action_indices = range(16)
+modality_config = {
     "video": ModalityConfig(
-        delta_indices=[0],
-        modality_keys=["video.external", "video.wrist"]
+        delta_indices=observation_indices,
+        modality_keys=video_keys,
     ),
     "state": ModalityConfig(
-        delta_indices=[0],
-        modality_keys=[
-            "state.arm_joints",
-            "state.gripper",
-        ],
+        delta_indices=observation_indices,
+        modality_keys=state_keys,
     ),
     "action": ModalityConfig(
-        delta_indices=[0],
-        modality_keys=[
-            "action.arm_joints",
-            "action.gripper",
-        ],
+        delta_indices=action_indices,
+        modality_keys=action_keys,
     ),
     "language": ModalityConfig(
-        delta_indices=[0],
-        modality_keys=["annotation.human.task_description"]
+        delta_indices=observation_indices,
+        modality_keys=language_keys,
     )
 }
+
+
+
+
+transforms = [
+    # video transforms
+    VideoToTensor(apply_to=video_keys),
+    VideoCrop(apply_to=video_keys, scale=0.95),
+    VideoResize(apply_to=video_keys, height=224, width=224, interpolation="linear"),
+    VideoColorJitter(
+        apply_to=video_keys,
+        brightness=0.3,
+        contrast=0.4,
+        saturation=0.5,
+        hue=0.08,
+    ),
+    VideoToNumpy(apply_to=video_keys),
+    # state transforms
+    StateActionToTensor(apply_to=state_keys),
+    StateActionTransform(
+        apply_to=state_keys,
+        normalization_modes={
+            "state.arm_joints" : "min_max",
+            "state.gripper" : "min_max",
+        }
+    ),
+    # action transforms
+    StateActionToTensor(apply_to=action_keys),
+    StateActionTransform(
+        apply_to=action_keys,
+        normalization_modes={
+            "action.arm_joints": "min_max",
+            "action.gripper": "min_max",
+        }
+    ),
+    # concat transforms
+    ConcatTransform(
+        video_concat_order=video_keys,
+        state_concat_order=state_keys,
+        action_concat_order=action_keys,
+    ),
+    GR00TTransform(
+        state_horizon=len(observation_indices), 
+        action_horizon=len(action_indices),
+        max_state_dim=64,
+        max_action_dim=32,
+    ),
+]
+
+composedModalityTform = ComposedModalityTransform(transforms=transforms)
+
 
 
 # 3. gr00t embodiment tag
 embodiment_tag = EmbodimentTag.NEW_EMBODIMENT
 
 # load the dataset
-dataset = LeRobotSingleDataset(DATA_PATH, modality_configs,  embodiment_tag=embodiment_tag)
+train_dataset = LeRobotSingleDataset(DATA_PATH, modality_config,  embodiment_tag=embodiment_tag, transforms=composedModalityTform)
+
+#print(train_dataset[0].keys())
+#print(train_dataset[0].values())
+
+# print the 7th data point
+resp = train_dataset[7]
+any_describe(resp)
+print(resp.keys())
+#exit(0)
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -77,8 +153,8 @@ model.compute_dtype = "bfloat16"
 model.config.compute_dtype = "bfloat16"
 model.to(device)
 
-output_dir = "/content/train_result/"    # CHANGE THIS ACCORDING TO YOUR LOCAL PATH
-per_device_train_batch_size = 8     # CHANGE THIS ACCORDING TO YOUR GPU MEMORY
+output_dir = "./train_result/"    # CHANGE THIS ACCORDING TO YOUR LOCAL PATH
+per_device_train_batch_size = 1     # CHANGE THIS ACCORDING TO YOUR GPU MEMORY
 max_steps = 100                      # CHANGE THIS ACCORDING TO YOUR NEEDS
 report_to = "wandb"
 dataloader_num_workers = 8
