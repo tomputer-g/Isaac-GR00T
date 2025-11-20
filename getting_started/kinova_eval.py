0b242d071e4daf5f7e46db7a33998182e25439a2
Ishita Gupta <gupta.ishita212@gmail.com>
2025-11-20 07:20:16 -0800

    Working training and evaluation script for the kinova dataset

--- getting_started/kinova_eval.py
+++ getting_started/kinova_eval.py
@@ -1,3 +1,5 @@
+from gr00t.utils.eval import calc_mse_for_single_trajectory
+import warnings
 from gr00t.utils.misc import any_describe
 from gr00t.data.dataset import LeRobotSingleDataset
 from gr00t.data.dataset import ModalityConfig
@@ -12,6 +14,7 @@ import torch
 from transformers import TrainingArguments
 
 from gr00t.experiment.runner import TrainRunner
+from gr00t.model.policy import Gr00tPolicy
 
 from gr00t.data.dataset import ModalityConfig
 from gr00t.data.transform.base import ComposedModalityTransform, ModalityTransform
@@ -29,8 +32,9 @@ from gr00t.data.transform.video import (
     VideoToTensor,
 )
 from gr00t.model.transforms import GR00TTransform
-# REPO_PATH is the path of the pip install gr00t repo and one level up
 REPO_PATH = os.path.dirname(os.path.dirname(gr00t.__file__))
+
+embodiment_tag = EmbodimentTag.NEW_EMBODIMENT
 DATA_PATH = os.path.join(REPO_PATH, "datasets/kinova_dataset_nov6")
 
 print("Loading dataset... from", DATA_PATH)
@@ -112,96 +116,27 @@ transforms = [
 
 composedModalityTform = ComposedModalityTransform(transforms=transforms)
 
-
-
-# 3. gr00t embodiment tag
-embodiment_tag = EmbodimentTag.NEW_EMBODIMENT
-
-# load the dataset
 train_dataset = LeRobotSingleDataset(DATA_PATH, modality_config,  embodiment_tag=embodiment_tag, transforms=composedModalityTform)
 
-#print(train_dataset[0].keys())
-#print(train_dataset[0].values())
-
-# print the 7th data point
-#resp = train_dataset[7]
-#any_describe(resp)
-#print(resp.keys())
-#exit(0)
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-from gr00t.model.gr00t_n1 import GR00T_N1_5
-
-BASE_MODEL_PATH = "nvidia/GR00T-N1.5-3B"
-TUNE_LLM = False            # Whether to tune the LLM
-TUNE_VISUAL = False          # Whether to tune the visual encoder
-TUNE_PROJECTOR = True       # Whether to tune the projector
-TUNE_DIFFUSION_MODEL = True # Whether to tune the diffusion model
-
-model = GR00T_N1_5.from_pretrained(
-    pretrained_model_name_or_path=BASE_MODEL_PATH,
-    tune_llm=TUNE_LLM,  # backbone's LLM
-    tune_visual=TUNE_VISUAL,  # backbone's vision tower
-    tune_projector=TUNE_PROJECTOR,  # action head's projector
-    tune_diffusion_model=TUNE_DIFFUSION_MODEL,  # action head's DiT
+finetuned_model_path = "./train_result/checkpoint-1000"
+finetuned_policy = Gr00tPolicy(
+    model_path=finetuned_model_path,
+    embodiment_tag = embodiment_tag,
+    modality_config=modality_config,
+    modality_transform=composedModalityTform,
+    device="cuda:0",
 )
 
-# Set the model's compute_dtype to bfloat16
-model.compute_dtype = "bfloat16"
-model.config.compute_dtype = "bfloat16"
-model.to(device)
+warnings.simplefilter("ignore", category=FutureWarning)
 
-output_dir = "./train_result/"    # CHANGE THIS ACCORDING TO YOUR LOCAL PATH
-per_device_train_batch_size = 32     # CHANGE THIS ACCORDING TO YOUR GPU MEMORY
-max_steps = 1000                      # CHANGE THIS ACCORDING TO YOUR NEEDS
-report_to = "wandb"
-dataloader_num_workers = 8
-
-training_args = TrainingArguments(
-    output_dir=output_dir,
-    run_name=None,
-    remove_unused_columns=False,
-    deepspeed="",
-    gradient_checkpointing=False,
-    bf16=True,
-    tf32=True,
-    per_device_train_batch_size=per_device_train_batch_size,
-    gradient_accumulation_steps=1,
-    dataloader_num_workers=dataloader_num_workers,
-    dataloader_pin_memory=False,
-    dataloader_persistent_workers=True,
-    optim="adamw_torch",
-    adam_beta1=0.95,
-    adam_beta2=0.999,
-    adam_epsilon=1e-8,
-    learning_rate=1e-4,
-    weight_decay=1e-5,
-    warmup_ratio=0.05,
-    lr_scheduler_type="cosine",
-    logging_steps=10.0,
-    num_train_epochs=300,
-    max_steps=max_steps,
-    save_strategy="steps",
-    save_steps=500,
-    save_total_limit=8,
-    report_to=report_to,
-    seed=42,
-    do_eval=True,
-    ddp_find_unused_parameters=False,
-    ddp_bucket_cap_mb=100,
-    torch_compile_mode=None,
+mse = calc_mse_for_single_trajectory(
+    finetuned_policy,
+    train_dataset,
+    traj_id=0,
+    modality_keys=["arm_joints", "gripper"],
+    steps=150,
+    action_horizon=16,
+    plot=True
 )
 
-
-experiment = TrainRunner(
-    train_dataset=train_dataset,
-    model=model,
-    training_args=training_args,
-)
-
-experiment.train()
-
-print("Done")
-
+print("MSE loss for trajectory 0:", mse)
